b2298cb2c2df29b518f0bd9bccd2d2e2fda25cf9
Maryuris Huete <141036519+Maryuris24@users.noreply.github.com>
2023-09-07 07:03:04 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 import certifi
 
-MONGO_URI = 'mongodb+srv://melisahuete:a1n2i3m4e5@app.m76bbob.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://Admin:admin1@cluster0.pchtjug.mongodb.net/?retryWrites=true&w=majority'
 ca = certifi.where()
-#mongodb+srv://Admin:admin1@cluster0.pchtjug.mongodb.net/?retryWrites=true&w=majority
 
 # definimos el método de conexión
 def dbConnection():
