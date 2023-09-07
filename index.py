19e642fdb892f6596418e9d25e247e860b871d19
Maryuris Huete <141036519+Maryuris24@users.noreply.github.com>
2023-09-07 06:50:44 -0500

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 # Desplega los datos de la colección
 @app.route('/')
-def home():
+def inicio():
     listado = db['listado']
     listadoReceived = listado.find()
     return render_template('index.html', listado = listadoReceived)
@@ -36,7 +36,7 @@ def addList():
             'estado' : estado
         })
 
-        return redirect(url_for('home'))
+        return redirect(url_for('inicio'))
     else:
         return notFound()   
 
@@ -68,7 +68,7 @@ def edit(list_name):
             'message' : list_name + 'actualizado corectamente'
         })
 
-        return redirect(url_for('home'))
+        return redirect(url_for('inicio'))
     else:
         return notFound()
 
@@ -77,7 +77,7 @@ def edit(list_name):
 def delete(list_name):
     listado = db['listado']
     listado.delete_one({'id' : list_name})
-    return redirect(url_for('home'))
+    return redirect(url_for('inicio'))
 
 #Método de error
 @app.errorhandler(404)
